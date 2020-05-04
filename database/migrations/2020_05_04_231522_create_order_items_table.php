5a3aa23205a0708f2682bcc074e81b2fe5be347c
unknown <raihan@techynaf.com>
2020-05-05 05:19:39 +0600

    Add order and order item table

---
+++ database/migrations/2020_05_04_231522_create_order_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('ip_address');
+            $table->bigInteger('user_id')->nullable();
+            $table->bigInteger('product_id');
+            $table->integer('product_quantity');
+            $table->float('total_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}

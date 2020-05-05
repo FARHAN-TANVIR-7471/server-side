218729b433be0972203fddf0b5924fb65d0116c2
unknown <raihan@techynaf.com>
2020-05-05 22:04:49 +0600

    Show single product

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Product;
 
+use App\Cart;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductResource extends JsonResource
@@ -12,31 +13,52 @@ class ProductResource extends JsonResource
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
+
+     
+    public function totalProduct()
+    {
+      
+       $carts = Cart::where('ip_address', request()->ip())->get();
+
+       $total_item = 0;
+
+       foreach ($carts as $cart)
+       {
+           $total_item += $cart->product_quantity; 
+       }
+
+       return $total_item;
+    }
+
+
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'price' => $this->price,
-            'discount' => $this->discount,
-            'discount' => $this->discount,
-            'totalPrice' => round((1 - ($this->discount/100)) * $this->price, 2),
-            'gender_id' => $this->gender_id,
-            'product_type_id' => $this->product_type_id,
-            'custom' => $this->custom,
-            'number' => $this->number ==0 ? 'Out of Stock' : $this->number,
-            'size' => $this->size,
-            'description' => $this->description,
-            'image' => $this->image,
-            'color' => $this->color,
-            'trend' => $this->trend,
-            'status' => $this->status,
+            'data' => [
+                'id' => $this->id,
+                'name' => $this->name,
+                'price' => $this->price,
+                'discount' => $this->discount,
+                'discount' => $this->discount,
+                'totalPrice' => round((1 - ($this->discount/100)) * $this->price, 2),
+                'gender_id' => $this->gender_id,
+                'product_type_id' => $this->product_type_id,
+                'custom' => $this->custom,
+                'number' => $this->number ==0 ? 'Out of Stock' : $this->number,
+                'size' => $this->size,
+                'description' => $this->description,
+                'image' => $this->image,
+                'color' => $this->color,
+                'trend' => $this->trend,
+                'status' => $this->status,
 
-            'rating' =>$this->reviews->count() > 0 ? round( $this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating',
+                'rating' =>$this->reviews->count() > 0 ? round( $this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating',
 
-            'href' => [
-                'reviews' => route('reviews.index',$this->id)
-            ]
-        ];
+                'href' => [
+                    'reviews' => route('reviews.index',$this->id)
+                ]
+            ],
+            'totalproduct' => $this->totalProduct(),    
+        ];  
     }
 }

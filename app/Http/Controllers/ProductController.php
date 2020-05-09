4aaad0d4d21c8670cc05ae651a551500be587ef0
Farhan Tanvir <farhan15-7471@diu.edu.bd>
2020-05-09 21:10:42 +0600

    product update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -104,7 +104,7 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $product->update($request->all());
     }
 
     /**
--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+	protected $fillable = [
+		'name','price','discount','gender_id','product_type_id','custom','number','size','description','image','color'
+	];
+
     public function reviews(){
 
     	return $this->hasMany(Review::class);
